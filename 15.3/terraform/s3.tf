7073cbc3ff7560a0386fdabd31c6349f9485918b
gorelovniko <gorelovniko@yandex.ru>
2025-12-18 23:42:06 +0500

    HW15.3 done

--- 15.3/terraform/s3.tf
+++ 15.3/terraform/s3.tf
@@ -6,12 +6,48 @@ locals {
   current_date = data.external.current_date.result.date
 }
 
+# Создание ключа KMS
+resource "yandex_kms_symmetric_key" "bucket-key" {
+  name              = "bucket-encryption-key"
+  description       = "Ключ для шифрования бакета Object Storage"
+  default_algorithm = "AES_256"
+  rotation_period   = "8760h" # 1 год
+}
+
 # Создание бакета Object Storage
 resource "yandex_storage_bucket" "student_bucket" {
 
   bucket     = "${var.bucket_name}-${local.current_date}"
   folder_id = var.yc_folder_id
+  # acl    = "private"
 
+server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = yandex_kms_symmetric_key.bucket-key.id
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  # Опционально: настройка версионирования для дополнительной безопасности
+  versioning {
+    enabled = true
+  }
+
+  # Опционально: политика блокировки удаления
+  lifecycle_rule {
+    id      = "protect-encrypted-data"
+    enabled = true
+    
+    expiration {
+      days = 0
+    }
+
+    noncurrent_version_expiration {
+      days = 30
+    }
+  }
 }
 
 resource "yandex_storage_bucket_iam_binding" "bucket-full-access" {
